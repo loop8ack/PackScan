d07964307ccb0729acf31252d36efd0dfe329696
Loop8ack <41995252+loop8ack@users.noreply.github.com>
2024-04-21 21:40:20 +0200

    Release - Push to nuget.org

--- build.ps1
+++
@@ -1,4 +0,0 @@
-dotnet pack -o artifacts -c Release ./src/PackScan.Analyzer
-dotnet pack -o artifacts -c Release ./src/PackScan.Defaults
-dotnet pack -o artifacts -c Release ./src/PackScan.Tool
-dotnet pack -o artifacts -c Release ./src/PackScan.PackagesProvider.Abstractions
